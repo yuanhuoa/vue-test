0c69152d0786d2acf0b01b8f927870f76ae1810a
yuanhuoa <1210433677@qq.com>
2018-05-30 15:23:45 +0800

    1

--- extension2.0/src/ts/constans.ts
+++ extension2.0/src/ts/constans.ts
@@ -3,33 +3,58 @@ export const DEALGO_SERVER_ADDR: string = "http://47.254.26.178/";
 /**
  * 主div
  */
-export const DIV_SHOW: string =
-`<div id="div_show">
+export const DIV_SHOW: string = `
+<div id="div_show">
   <div class="_header_show">
-    <p class="_title_show">One Piece</p>
+    <img id="_title_img" class="_title_img"/>
     <span id="_close_show" class="_close_show">×</span>
   </div>
   <div id="_content_show" class="_content_show">
-    <p>123</p>
-    <p>123</p>
-    <p>123</p>
-    <p>123</p>
-    <p>123</p>
-    <p>123</p>
-    <p>123</p>
-    <p>123</p>
-    <p>123</p>
-    <p>123</p>
-    <p>123</p>
-    <p>123</p>
-    <p>123</p>
-    <p>123</p>
-    <p>123</p>
-    <p>123</p>
-    <p>123</p>
   </div>
   <div id="_footer_show">
-    <div class="_footer_left_show">left</div>
-    <div class="">right</div>
+    <div id="_footer_left_show" class="_footer_left_show">left</div>
+    <div id="_footer_right_show" class="_footer_left_show">right</div>
   </div>
+</div>`;
+
+export const FIRST_PAGE: string =`
+<div id="_first_page">
+  <p>123</p>
+  <p>123</p>
+  <button id="logina" type="button">FaceBook登录</button>
+  <p>123</p>
+  <p>123</p>
+  <p>123</p>
+  <p>123</p>
+  <p>123</p>
+  <p>123</p>
+  <p>123</p>
+  <p>123</p>
+  <p>123</p>
+  <p>123</p>
+  <p>123</p>
+  <p>123</p>
+  <p>123</p>
+  <p>123</p>
+</div>`;
+
+export const SECOND_PAGE: string =`
+<div id="_second_page">
+  <p>456</p>
+  <p>456</p>
+  <p>456</p>
+  <p>456</p>
+  <p>456</p>
+  <p>456</p>
+  <p>456</p>
+  <p>456</p>
+  <p>456</p>
+  <p>456</p>
+  <p>456</p>
+  <p>456</p>
+  <p>456</p>
+  <p>456</p>
+  <p>456</p>
+  <p>456</p>
+  <p>456</p>
 </div>`;
--- extension2.0/src/ts/popup.ts
+++ extension2.0/src/ts/popup.ts
@@ -1,8 +1,9 @@
 import "../static/css/popup.css";
 import $ = require("../static/js/jquery-3.3.1.min.js");
 
+var extension = null;
 import {
-  DIV_SHOW
+  DIV_SHOW, FIRST_PAGE, SECOND_PAGE
 } from "./constans";
 
 function addDocumentUrlListener() {
@@ -10,12 +11,27 @@ function addDocumentUrlListener() {
   $("#_close_show").click(function () {
     $("#div_show").css("display", "none")
   })
+
+  // 切换页面
+  $("#_footer_left_show").click(function () {
+    if ($("#_first_page")[0] != undefined) return
+    $("#_second_page").remove()
+    $("#_content_show").html(FIRST_PAGE)
+  })
+  $("#_footer_right_show").click(function () {
+    if ($("#_second_page")[0] != undefined) return
+    $("#_first_page").remove()
+    $("#_content_show").html(SECOND_PAGE)
+  })
 }
 addDocumentUrlListener();
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  extension = sender
+  $("#_title_img").attr("src", 'chrome-extension://'+ extension['id'] +'/static/icons/title.png')
+  $("#_content_show").html(FIRST_PAGE)
   if(message.title === "change-show"){
-    // 显示主div   chrome-extension://acihopjfdoofljkkkkijpfafmglgjdam/static/icons/icon_16.png
+    // 显示主div
     if($("#div_show").css("display") == "none"){
       $("#div_show").css("display", "block")
     } else {
@@ -32,3 +48,47 @@ function playSuccessAudio() {
   audio.play();
 }
 
+/**
+ * 浏览器识别
+ */
+function getBrowserInfo(){
+  var ua = navigator.userAgent.toLocaleLowerCase();
+  var browserType=null;
+  if (ua.match(/msie/) != null || ua.match(/trident/) != null) {
+    browserType = "IE";
+  } else if (ua.match(/firefox/) != null) {
+    browserType = "火狐";
+  }else if (ua.match(/ubrowser/) != null) {
+    browserType = "UC";
+  }else if (ua.match(/opera/) != null) {
+    browserType = "欧朋";
+  } else if (ua.match(/bidubrowser/) != null) {
+    browserType = "百度";
+  }else if (ua.match(/metasr/) != null) {
+    browserType = "搜狗";
+  }else if (ua.match(/tencenttraveler/) != null || ua.match(/qqbrowse/) != null) {
+    browserType = "QQ";
+  }else if (ua.match(/maxthon/) != null) {
+    browserType = "遨游";
+  }else if (ua.match(/chrome/) != null) {
+    var is360 = _mime("type", "application/vnd.chromium.remoting-viewer");
+    function _mime(option, value) {
+      var mimeTypes = navigator.mimeTypes;
+      for (var mt in mimeTypes) {
+        if (mimeTypes[mt][option] == value) {
+          return true;
+        }
+      }
+      return false;
+    }
+    if(is360){
+      browserType = '360';
+    }else{
+      browserType = '谷歌';
+      $('html').css("zoom",".80");
+    }
+  }else if (ua.match(/safari/) != null) {
+    browserType = "Safari";
+  }
+  console.log(browserType)
+}
